6d032ff3277eabe0b512229d098f9df06c637274
KayoTv0 <158571411+KayoTv0@users.noreply.github.com>
2026-04-14 21:52:00 +0200

    Delete src directory

--- src/logic/batch.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_batch(path, cwd):
-    try:
-        subprocess.run([path], cwd=cwd, check=True, shell=True)
-        return True
-    except:
-        return False
--- src/main.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from ui.app import SaveEditorApp
-
-if __name__ == "__main__":
-    app = SaveEditorApp()
-    app.mainloop()
